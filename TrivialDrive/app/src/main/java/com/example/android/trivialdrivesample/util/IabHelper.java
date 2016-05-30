9b689b9b25cf7cdf435d3dcca8dfe79b948cf900
wangzhoqi <wangzhoqi@users.noreply.github.com>
2016-05-30 15:52:51 +0800

    Fix getPurchases crash
    When service disconnected the mService = null and in function int queryPurchases(Inventory inv, String itemType) call mService.getPurchases will crash

--- TrivialDrive/app/src/main/java/com/example/android/trivialdrivesample/util/IabHelper.java
+++ TrivialDrive/app/src/main/java/com/example/android/trivialdrivesample/util/IabHelper.java
@@ -224,6 +224,7 @@ public class IabHelper {
             @Override
             public void onServiceDisconnected(ComponentName name) {
                 logDebug("Billing service disconnected.");
+                mSetupDone = false;
                 mService = null;
             }
 
